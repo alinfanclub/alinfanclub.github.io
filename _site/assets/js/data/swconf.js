3cc49a3472277161a65637f5a9071cf36a1c3ed7
e759ksh <e759ksh@naver.com>
2024-04-23 15:01:00 +0900

    first commit

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1713782063',resources: [
+    cacheName: 'chirpy-1713852010',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
