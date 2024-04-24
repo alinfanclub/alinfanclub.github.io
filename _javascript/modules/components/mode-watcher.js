996232b73d60d0dc335e1467c6462ecbaa813497
e759ksh <e759ksh@naver.com>
2024-04-24 16:15:04 +0900

    utteranc add and npm install and fix yml file

--- _javascript/modules/components/mode-watcher.js
+++ _javascript/modules/components/mode-watcher.js
@@ -1,14 +1,14 @@
 /**
  * Add listener for theme mode toggle
  */
-const toggle = document.getElementById('mode-toggle');
+const toggle = document.getElementById("mode-toggle");
 
 export function modeWatcher() {
   if (!toggle) {
     return;
   }
 
-  toggle.addEventListener('click', () => {
+  toggle.addEventListener("click", () => {
     modeToggle.flipMode();
   });
 }
