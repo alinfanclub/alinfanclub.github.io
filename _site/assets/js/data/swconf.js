3bff3fe2c117fb4ecae3d87a2eb93b1ff2b0fa2a
e759ksh <e759ksh@naver.com>
2024-04-24 17:12:14 +0900

    re build bundle

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1713946058',resources: [
+    cacheName: 'chirpy-1713946313',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
